7a1b8194d2c0b8d8856f0c8ed20fc6516a848b04
Smile <90579042+smile0101@users.noreply.github.com>
2026-09-11 16:31:34 +0900

    Update ETF.py

--- ETF.py
+++ ETF.py
@@ -105,7 +105,7 @@ def fetch_naver_frgn_page(code):
 
 dfv = load_data(code)
 dfc = dfv.tail(20).copy()
-dff = _fetch_naver_frgn_page(code)
+dff = fetch_naver_frgn_page(code)
 dft = dfc[['Date','Close','Change']].merge(dff[['dd', '외국인', '기관', '개인', '보유율']],
     left_on='Date', right_on='dd', how='left').drop(columns='dd')
 cols = ['외국인', '기관', '개인']
