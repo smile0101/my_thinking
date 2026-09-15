1169451906c1281dd6c15aae319d3217599fde5a
Smile <90579042+smile0101@users.noreply.github.com>
2026-09-15 09:51:39 +0900

    Restore imports and functionality in show.py

--- show.py
+++ show.py
@@ -67,7 +67,7 @@ for idx, row in dfv.iterrows():
     with col1:
         st.image(f"https://ssl.pstatic.net/imgfinance/chart/item/area/week/{code}.png")  
         st.markdown( f' [{ch}]_&nbsp;&nbsp;{SCHD}', unsafe_allow_html=True)
-        st.markdown(f'{CD} / (BD)원, ({RD}) '
+        st.markdown(f'{CD} / {BD}원, ({RD}) '
             f'<span style="margin-left:10px;"></span>'
             f'<a href="https://stock.naver.com/domestic/stock/{code}/price" target="_blank">네이버 </a>',  unsafe_allow_html=True )
 
